94e5f4ddcf3cedb9596bebad68651d10e651c03a
nashthecoder <naijeria@gmail.com>
2024-11-10 18:37:30 +0300

    styled landing page and devise viewws

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,6 @@
 class LandingController < ApplicationController
   def index
-    @mentors = Mentor.limit(6) # This will fecth 6 mentors
-    @mentees = Mentee.limit(6) # This will fecth 6 mentees
+    @mentors = Mentor.limit(3) # This will fecth 6 mentors
+    @mentees = Mentee.limit(3) # This will fecth 6 mentees
   end
 end
